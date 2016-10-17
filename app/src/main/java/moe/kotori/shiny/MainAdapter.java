04717f2d3eebcbb6bb2389097f0539b9146a9ff9
Ted Zyzsdy <Ted Zyzsdy>
2016-10-17 15:44:23 +0800

    0.1.4

--- app/src/main/java/moe/kotori/shiny/MainAdapter.java
+++ app/src/main/java/moe/kotori/shiny/MainAdapter.java
@@ -1,14 +1,18 @@
 package moe.kotori.shiny;
 
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 import moe.kotori.shiny.data.MessageItem;
 
@@ -50,13 +54,13 @@ public class MainAdapter extends RecyclerView.Adapter<MainAdapter.MyViewHolder> 
 
     @Override
     public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        MyViewHolder holder = new MyViewHolder(inflater.inflate(R.layout.fragment_info_list_card, parent, false));
+        MyViewHolder holder = new MyViewHolder(inflater.inflate(R.layout.fragment_info_list_card, parent, false), null);
         return holder;
     }
 
     @Override
     public void onBindViewHolder(MyViewHolder holder, int position) {
-        MessageItem item = messageItems.get(position);
+        final MessageItem item = messageItems.get(position);
         holder.content.setText(item.getContent());
         holder.source.setText(item.getPublisher() + "#" + item.getHash());
         String[] levelChart = {
@@ -69,6 +73,20 @@ public class MainAdapter extends RecyclerView.Adapter<MainAdapter.MyViewHolder> 
         };
         holder.level.setText(levelChart[item.getLevel()]);
         holder.title.setText(item.getTitle());
+
+        holder.title.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String link = item.getLink();
+                if(link != null && !Objects.equals(link, "")) {
+                    Intent intent = new Intent();
+                    intent.setAction("android.intent.action.VIEW");
+                    Uri content_url = Uri.parse(link);
+                    intent.setData(content_url);
+                    context.startActivity(intent);
+                }
+            }
+        });
     }
 
     @Override
@@ -76,18 +94,31 @@ public class MainAdapter extends RecyclerView.Adapter<MainAdapter.MyViewHolder> 
         return messageItems.size();
     }
 
-    class MyViewHolder extends RecyclerView.ViewHolder {
+    class MyViewHolder extends RecyclerView.ViewHolder implements View.OnLongClickListener{
         TextView title;
         TextView level;
         TextView source;
         TextView content;
+        OnItemTitleClickListener titleClickListener;
 
-        public MyViewHolder(View itemView) {
+        public MyViewHolder(View itemView, OnItemTitleClickListener titleClickListener) {
             super(itemView);
+            this.titleClickListener = titleClickListener;
             title = (TextView) itemView.findViewById(R.id.item_data_title);
             level = (TextView) itemView.findViewById(R.id.item_data_level);
             source = (TextView) itemView.findViewById(R.id.item_data_source);
             content = (TextView) itemView.findViewById(R.id.item_data_content);
+
+            itemView.setOnLongClickListener(this);
+        }
+
+        @Override
+        public boolean onLongClick(View v) {
+            if(titleClickListener != null){
+                titleClickListener.onItemTitleClick(v, getLayoutPosition());
+            }
+            return true;
         }
     }
 }
+
